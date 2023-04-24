37146948bb07b7c4e18d3a62d9d24654b664bc58
sridhar vumma <50115187+sridatascience@users.noreply.github.com>
2023-04-24 12:18:33 +0530

    added volatile qualifier

--- PIC18F47K42_AutomatedProject.X/newmain.c
+++ PIC18F47K42_AutomatedProject.X/newmain.c
@@ -8,18 +8,18 @@
 
 #include <xc.h>
 #pragma config WDTE = OFF        // WDT operating mode (WDT enabled regardless of sleep; SWDTEN is ignored)
-uint8_t m =10;
-uint8_t add(uint8_t n, uint8_t o, uint8_t p ){
+volatile uint8_t m =10;
+volatile uint8_t add(volatile uint8_t n, volatile uint8_t o, volatile uint8_t p ){
    return n+o+p;
 }
 void main() {
-   uint8_t a = 0x05;
+   volatile uint8_t a = 0x05;
    LATA = a;
-   uint8_t c = 0x1F;
+   volatile uint8_t c = 0x1F;
    TRISA = c;
-   uint8_t b =0x08;
+   volatile uint8_t b =0x08;
    LATB = b;
-   uint8_t t = add(a,b,c);
+   volatile uint8_t t = add(a,b,c);
    return;
 }
 
